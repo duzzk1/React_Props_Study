f414267c77ce466a852b0832610c649b697c66d0
Eduardo Santos <115682186+duzzk1@users.noreply.github.com>
2023-01-23 22:12:53 -0300

    Styling my webpage, with waves background

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import duzzk from "../../img/duzzk.png";
 import "../../css/template.css";
 import { TypeAnimation } from 'react-type-animation';
 
- 
+
 
 const Home = (props) => {
   return (
@@ -10,18 +10,24 @@ const Home = (props) => {
       <div className="home">
         <div className="homeApresentation">
           <h1>Eu sou o <span><TypeAnimation
-      sequence={['Eduardo',1000,
-      "Duzzk",1000
-      ]}
-      wrapper="span"
-      cursor={true}
-      repeat={Infinity}
-      speed={1}
-    /></span>
+            sequence={['Eduardo', 1000,
+              "Duzzk", 1000
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            speed={1}
+          /></span>
           </h1>
           <p>{props.desc}</p>
         </div>
         <div className="imgHome"><img src={duzzk} alt="" /></div>
+      </div>
+      <div className="homeSecond">
+
+      </div>
+      <div className="homeThird">
+
       </div>
     </>
   )
